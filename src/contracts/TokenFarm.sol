99973325cba25848e58d7d84530c3dc02d11c81c
blacktanktop <blacktanktopme@gmail.com>
2022-10-10 00:10:35 +0900

    Add unstaking function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -6,6 +6,7 @@ import "./MockDaiToken.sol";
 
 contract TokenFarm{
     string public name = "Dapp Token Farm";
+    address public owner;
     DappToken public dappToken;
     DaiToken public daiToken;
 
@@ -24,6 +25,7 @@ contract TokenFarm{
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
     // 1.ステーキング機能を作成する
     function stakeTokens(uint _amount) public {
@@ -42,5 +44,36 @@ contract TokenFarm{
         // 9. ステーキングステータスの更新
         isStaking[msg.sender] = true;
         hasStaked[msg.sender] = true;
+    }
+    // ----- 追加する機能 ------ //
+    //2.トークンの発行機能
+    function issueTokens() public {
+        // Dapp トークンを発行できるのはあなたのみであることを確認する
+        require(msg.sender == owner, "caller must be the owner");
+
+        // 投資家が預けた偽Daiトークンの数を確認し、同量のDappトークンを発行する
+        for(uint i=0; i<stakers.length; i++){
+            // recipient は Dapp トークンを受け取る投資家
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0){
+                dappToken.transfer(recipient, balance);
+            }
+        }
+    }
+
+    //　3.アンステーキング機能
+    // * 投資家は、預け入れた Dai を引き出すことができる
+    function unstakeTokens() public {
+        // 投資家がステーキングした金額を取得する
+        uint balance = stakingBalance[msg.sender];
+        // 投資家がステーキングした金額が0以上であることを確認する
+        require(balance > 0, "staking balance cannot be 0");
+        // 偽の Dai トークンを投資家に返金する
+        daiToken.transfer(msg.sender, balance);
+        // 投資家のステーキング残高を0に更新する
+        stakingBalance[msg.sender] = 0;
+        // 投資家のステーキング状態を更新する
+        isStaking[msg.sender] = false;
     }
 }
